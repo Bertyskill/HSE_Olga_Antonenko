63ab12bb0182ac8a1dde429b7a92b780409c326a
Bertyskill <e.wagner.v@gmail.com>
2024-05-13 21:36:21 +0300

    ..

---
+++ Lesson 6/дз.12.py
@@ -0,0 +1,4 @@
+"""На сайте https://kad.arbitr.ru/ - openapi
+На сайте https://fedresurs.ru/ -  cadesplugin_api
+На сайте https://5ka.ru/ -  apikey
+На сайте https://www.nasdaq.com/ -  api.nasdaq"""
